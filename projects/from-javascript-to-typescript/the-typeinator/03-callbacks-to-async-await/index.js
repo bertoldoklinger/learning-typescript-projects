fd82bab8345ad45db95107d5fdfdfc93c295da7a
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-11-16 14:37:14 -0300

    Refactor callback-based speak function to use async/await

--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,21 +1,17 @@
 async function checkEmotion(knownEmotions, emotion) {
-  // Simulate database processing time by waiting a second...
-  setTimeout(() => {
-    knownEmotions.has(emotion)
-  }, 1000);
+	// Simulate database processing time by waiting a second...
+	await new Promise((resolve) => {
+		return setTimeout(resolve, 1000);
+	});
+	return knownEmotions.has(emotion);
 }
 
-function speak(knownEmotions, newEmotion, phrase, callback) {
-  checkEmotion(knownEmotions, newEmotion, (hasEmotion) => {
-    if (hasEmotion) {
-      callback(`"${phrase}" (${newEmotion})`);
-    } else {
-      callback(
-        undefined,
-        new Error(`Does not compute. I do not understand ${newEmotion}.`)
-      );
-    }
-  });
+async function speak(knownEmotions, newEmotion, phrase) {
+	if (!(await checkEmotion(knownEmotions, newEmotion))) {
+		throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
+	}
+
+	return `"${phrase}" (${newEmotion})`;
 }
 
 module.exports.checkEmotion = checkEmotion;
